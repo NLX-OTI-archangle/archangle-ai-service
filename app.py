3a319b830b60a447056883bae7b8a999ab651b78
alexchtst <acinatra@gmail.com>
2025-08-17 20:25:31 +0700

    feat: add archangle header in project

--- app.py
+++ app.py
@@ -4,22 +4,29 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from gradio_client import Client
 
-# Load .env
 load_dotenv()
 
 HUGGING_FACE_EXAMPLE_LINEAR_REGRESSION = os.getenv("HUGGING_FACE_EXAMPLE_LINEAR_REGRESSION")
+FRONTEND_DOMAIN_APP = os.getenv("DOMAIN_APP", "http://localhost:3000")
+HEADER_NAME = os.getenv("HEADER_NAME")
+HEADER_VALUE = os.getenv("HEADER_VALUE")
 
-# Flask init
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:3000", FRONTEND_DOMAIN_APP])
 
-# Connect ke Hugging Face Space pakai gradio_client
 client = Client(HUGGING_FACE_EXAMPLE_LINEAR_REGRESSION)
-
 
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
+        header = request.headers.get(HEADER_NAME)
+    
+        if not header :
+            return jsonify({"message": "Need Header to get service"}, 400)
+        
+        if header != HEADER_VALUE:
+            return jsonify({"message": "Wrong header value"}, 400)
+
         data = request.get_json()
         input_value = data.get("input")
 
@@ -28,15 +35,15 @@ def predict():
 
         result = client.predict(input_value, api_name="/predict")
 
-        return jsonify({"prediction": result})
+        return jsonify({"message": "success to make a result", "result": result})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Flask + Gradio service running"})
+    return jsonify({"message": "Archangle AI API service is Ready"})
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5555, debug=True)
